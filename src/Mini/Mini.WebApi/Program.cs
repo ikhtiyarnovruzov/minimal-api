43ca8d2d1d464bcb5bdda75bc76ec875f4c2832f
1khtiyar <ixty.now@bk.ru>
2022-09-14 23:14:34 +0400

    - Add translatorService; - Add translation endpoint;

--- src/Mini/Mini.WebApi/Program.cs
+++ src/Mini/Mini.WebApi/Program.cs
@@ -1,7 +1,16 @@
+using Mini.WebApi.Utils;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<TranslationService>();
 
 var app = builder.Build();
 
 app.MapGet("/", () => Results.Ok("Mini"));
+
+app.MapGet("/translate/{key}", async (string key, TranslationService facade) =>
+    await facade.TranslateAsync(key) is string translatedKey
+        ? Results.Ok(translatedKey)
+        : Results.StatusCode(StatusCodes.Status500InternalServerError));
 
 app.Run();
---
+++ src/Mini/Mini.WebApi/Utils/TranslationService.cs
@@ -0,0 +1,10 @@
+﻿namespace Mini.WebApi.Utils
+{
+    public class TranslationService
+    {
+        public async Task<string> TranslateAsync(string key)
+        {
+            return await Task.FromResult("Translated " + key);
+        }
+    }
+}
